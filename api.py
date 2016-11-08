a21fdbbadc33b0b2949140819b52125ba5e5565e
Arthur Yip <arthuryip723@gmail.com>
2016-11-07 23:44:08 -0500

    to my code

--- api.py
+++ api.py
@@ -10,41 +10,43 @@ from itsdangerous import (TimedJSONWebSignatureSerializer
 # initialization
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/flask1'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 # extensions
 db = SQLAlchemy(app)
 auth = HTTPBasicAuth()
 
+from models import User
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(32), index=True)
-    password_hash = db.Column(db.String(64))
+# class User(db.Model):
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(32), index=True)
+#     password_hash = db.Column(db.String(64))
 
-    def hash_password(self, password):
-        self.password_hash = pwd_context.encrypt(password)
+#     def hash_password(self, password):
+#         self.password_hash = pwd_context.encrypt(password)
 
-    def verify_password(self, password):
-        return pwd_context.verify(password, self.password_hash)
+#     def verify_password(self, password):
+#         return pwd_context.verify(password, self.password_hash)
 
-    def generate_auth_token(self, expiration=600):
-        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
-        return s.dumps({'id': self.id})
+#     def generate_auth_token(self, expiration=600):
+#         s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+#         return s.dumps({'id': self.id})
 
-    @staticmethod
-    def verify_auth_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
-        try:
-            data = s.loads(token)
-        except SignatureExpired:
-            return None    # valid token, but expired
-        except BadSignature:
-            return None    # invalid token
-        user = User.query.get(data['id'])
-        return user
+#     @staticmethod
+#     def verify_auth_token(token):
+#         s = Serializer(app.config['SECRET_KEY'])
+#         try:
+#             data = s.loads(token)
+#         except SignatureExpired:
+#             return None    # valid token, but expired
+#         except BadSignature:
+#             return None    # invalid token
+#         user = User.query.get(data['id'])
+#         return user
 
 
 @auth.verify_password
